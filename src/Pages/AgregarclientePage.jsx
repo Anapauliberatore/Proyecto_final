812b321ab8f9fcbae1b6829583f2bff4db49879d
Laura43773 <lauramargaritacopa@gmail.com>
2025-07-01 16:18:28 -0300

    Agrego archivo AgregarclientePage.jsx

---
+++ src/Pages/AgregarclientePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Agregarcliente from '../Components/CONSTRUCTORACRUD/Agregarcliente'
+import Footer from '../Components/Footer'
+import "../css/Agregarcliente.css";
+
+const Crearcliente = () => {
+
+  return (
+    <div className="fondo-cliente">
+      <div style={{ height: "59px" }}></div>
+
+<Agregarcliente/>
+
+<div style={{ height: "59px" }}></div>
+<Footer/>
+
+
+    </div>
+    
+  
+  )
+}
+
+export default Crearcliente
