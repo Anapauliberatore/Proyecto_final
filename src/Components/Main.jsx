d21d977167c4eff41473425e9cbf50a787d27991
Anapauliberatore <anitaliberatore2000@gmail.com>
2025-07-01 20:56:44 -0300

    srreglo final

---
+++ src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import Carrusel from "../Components/Carrusel";
+import Servicios from "../Components/Servicios";
+import Vision from "../Components/Vision";'react'
+
+const Main = () => {
+  return (
+    <div>
+    <Carrusel />
+    <br />
+    <br />
+    <br />
+    <Vision />
+   
+    <br />
+    <Servicios />
+    </div>
+  )
+}
+
+export default Main
