b0f9d188d5461e5eb58bae64a83a8028b791c139
Anapauliberatore <anitaliberatore2000@gmail.com>
2025-07-01 15:47:26 -0300

    cambios en el app y json

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,63 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import { HOME,EDITARCLIENTE,EDITARCONSTRUCCION }   from "./Routers/router"
-import Editarcliente from "./Components/CONSTRUCTORACRUD/Editarcliente"
-import Editarconstruccion from "./Components/CONSTRUCTORACRUD/Editarconstruccion"
-import Home from './Pages/Home'
 
 
-function App() {
-  
+
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { TABLACONSTRUCCIONES, HOME, DESARROLLOSPAGE, INVERSORESPAGE,  TABLACLIENTE, LOGIN, AGREGARCLIENTEPAGE, EDITARCLIENTEPAGE, NOSOTROSPAGE, AGREGARCONSTRUCCIONPAGE, EDITARCONSTRUCCIONPAGE, VERCONSTRUCCIONPAGE, VERCLIENTEPAGE } from "./Routers/router";
+
+import EditarclientePage from "./Pages/EditarclientePage";
+import NosotrosPage from "./Pages/NosotrosPage";
+import EditarconstruccionPage from "./Pages/EditarconstruccionPage";
+import LoginPage from "./Pages/LoginPage";
+import Home from "./Pages/Home";
+import AgregarclientePage from "./Pages/AgregarclientePage";
+import AgregarconstruccionPage from "./Pages/AgregarconstruccionPage";
+import VerconstruccionPage from "./Pages/VerconstruccionPage";
+import VerclientePage from "./Pages/VerclientePage";
+import TablaCliente from "./Pages/TablaCliente";
+
+import DesarrollosPage from "./Pages/DesarrollosPage";
+import InversoresPage from "./Pages/InversoresPage";
+import TablaConstruccion from "./Pages/TablaConstruccion";
+
+import Header from "./Components/Header";
+import { LoginPermisoProvider } from "./Components/LoginPermisoContext";
+
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderRoutes = [LOGIN];
 
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path= {EDITARCLIENTE} element= {<Editarcliente/>} />
-      <Route path= {EDITARCONSTRUCCION} element= {<Editarconstruccion/>} />
-      <Route path= {HOME} element= {<Home/>} />
-      
+    <>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path={TABLACLIENTE} element={<TablaCliente />} />
+       
+        <Route path={TABLACONSTRUCCIONES} element={<TablaConstruccion />} />
+        <Route path={DESARROLLOSPAGE} element={<DesarrollosPage />} />
 
+       
+        <Route path={INVERSORESPAGE} element={<InversoresPage />} />
+        <Route path={AGREGARCLIENTEPAGE} element={<AgregarclientePage />} />
+        <Route path={AGREGARCONSTRUCCIONPAGE} element={<AgregarconstruccionPage />} />
 
-
-
-
-
-
-
-
-
-
-
-    </Routes>
-     
-    </BrowserRouter>
-  )
+        <Route path={NOSOTROSPAGE} element={<NosotrosPage />} />
+        <Route path={EDITARCONSTRUCCIONPAGE} element={<EditarconstruccionPage />} />
+        <Route path={VERCONSTRUCCIONPAGE} element={<VerconstruccionPage />} />
+        <Route path={VERCLIENTEPAGE} element={<VerclientePage />} />
+        <Route path={HOME} element={<Home />} />
+        <Route path={LOGIN} element={<LoginPage />} />
+        <Route path={EDITARCLIENTEPAGE} element={<EditarclientePage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <LoginPermisoProvider>
+        <AppContent />
+      </LoginPermisoProvider>
+    </BrowserRouter>
+  );
+}
